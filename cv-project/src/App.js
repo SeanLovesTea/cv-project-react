4313de184cf1262da5c582a81911dcbb9218a212
SeanLovesTea <sean.m.concannon@gmail.com>
2023-03-03 10:28:11 +0000

    Setup Form components and converted to class

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,25 +1,32 @@
+import  React from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Form from './components/form'
+import FormPreview from './components/form-preview'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(){
+    super()
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
---
+++ cv-project/src/components/form.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+  render() {
+    return(
+      <h1>Hello</h1>
+    )
+  }
+}
